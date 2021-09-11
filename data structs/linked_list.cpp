41107121a7f563d328df0dec534c512d9f5609ec
Rodrigo Mota <raafm@cin.ufpe.br>
2021-09-10 21:05:15 -0300

    debugged

--- data structs/linked_list.cpp
+++ data structs/linked_list.cpp
@@ -6,18 +6,18 @@ using namespace std;
 template <typename T>
 struct Node {
     T data;
-    Node* next;
-    Node* prev;
-    Node()next(NULL){};
-    Node(T data): data(data), next(NULL){};
-    Node(T data, Node* nextNode): data(data), next(nextNode), prev(NULL){};
-    Node(T data, Node* nextNode,Node* prevNode):  data(data), next(nextNode), prev(prevNode){};
+    Node<T>* next;
+    Node<T>* prev;
+    Node():next(NULL),prev(NULL) {};
+    Node(T data): data(data), next(NULL), prev(NULL) {};
+    Node(T data, Node<T>* nextNode): data(data), next(nextNode), prev(NULL){};
+    Node(T data, Node<T>* nextNode,Node<T>* prevNode):  data(data), next(nextNode), prev(prevNode){};
 };
 
 template <typename T>
 void link(Node<T>* node1, Node<T>* node2){
-    node1->next = node2;
-    node2->prev = node1;
+    if(node1)node1->next = node2;
+    if(node2)node2->prev = node1;
 }
 
 template <typename T>
@@ -26,10 +26,20 @@ class linked_list{
     protected:
 
         int length = 0; 
-        Node* head = NULL;
-        Node* tail = NULL;
+        Node<T>* head = NULL;
+        Node<T>* tail = NULL;
 
     public:
+        ~linked_list<T>()
+        {   
+            Node<T>* temp = head;
+            while (temp)
+            {
+                temp = temp->next;
+                free(head);
+                head = temp;
+            }      
+        }
         int size();
         void push_front(T new_data);
         void push_back (T new_data);
@@ -43,7 +53,7 @@ class linked_list{
 }; 
 
 template<typename T>
-int size() 
+int linked_list<T> :: size() 
 { 
     return length;
 }
@@ -53,11 +63,13 @@ void linked_list<T> :: push_front(T new_data)
 {
     if(head)
     {
-        link(head , new Node(new_data));
+        Node<T>*new_node = new Node<T>(new_data);
+        link(new_node,head);
+        head = new_node;
     }
     else
     {
-        tail = head = new Node(new_data);
+        tail = head = new Node<T>(new_data);
     }
     length++;
 }
@@ -67,28 +79,29 @@ void linked_list<T> :: push_back(T new_data)
 {
     if(head)
     {
-        link(tail, new Node(new_data));
+        link(tail, new Node<T>(new_data));
         tail = tail->next;
     }
     else
     {
-        tail = head = new Node(new_data);
+        tail = head = new Node<T>(new_data);
     }
     length++;
 }
-
-
 
 template<typename T>
 void linked_list<T> :: pop_front()
 {
     if(head)
     {
-        Node*temp = head->next;
+        Node<T>*nextNode = head->next;
         free(head);
-        head = temp;
-        length--;
+        head = nextNode;
+        if(nextNode)head->prev = NULL;   
+        else head =tail = NULL;
+        length--; 
     }
+    else cout<<"Empty\n";
 }
 
 template<typename T>
@@ -96,13 +109,17 @@ void linked_list<T> :: pop_back()
 {
     if(head)
     {
-        Node* previous = tail->prev;
+        Node<T>* previous = tail->prev;
         free(tail);
         if(previous)
+        {
             tail = previous;
+            tail->next = NULL;
+        }
         else tail = head = NULL;
         length--;
     }
+    else cout<<"Empty\n";
 }
 
 template<typename T>
@@ -120,21 +137,91 @@ T linked_list<T> :: last()
 template<typename T>
 void linked_list<T> :: print_list()
 {
-    Node* printer = head;
+    Node<T>* printer = head;
     while(printer)
     {
         cout<<printer->data<<" -> ";
         printer = printer->next;
     }
+    cout<<"NULL\n";
 }
 
 template<typename T>
 void linked_list<T> :: print_backwards()
 {
-    Node* printer = tail;
-    while(head)
+    Node<T>* printer = tail;
+    while(printer)
     {
         cout<<printer->data<<" -> ";
         printer = printer->prev;
     }
+    cout<<"NULL\n";
+}
+
+void options()
+{
+    cout<<"0 exit\n";
+    cout<<"1 push_front\n";
+    cout<<"2 push_back\n";
+    cout<<"3 pop_front\n";
+    cout<<"4 pop_back\n";
+    cout<<"5 first\n";
+    cout<<"6 last\n";
+    cout<<"7 print_list\n";
+    cout<<"8 print_backwards\n";    
+}
+
+int main(){
+    linked_list<int> test_list;
+    int choice = 1;
+    while(choice)
+    {
+        options();
+        cout<<"choose: ";
+        cin>>choice;
+        int number;
+        switch(choice) 
+        {
+            case 0:
+                break;
+
+            case 1:
+                cout<<"number: ";
+                cin>>number;
+                test_list.push_front(number);
+                break;
+
+            case 2:
+                cout<<"number: ";
+                cin>>number;
+                test_list.push_back(number);
+                break;
+            case 3:
+                test_list.pop_front();
+                break;
+
+            case 4:
+                test_list.pop_back();
+                break;
+
+            case 5:
+                test_list.first();
+                break;
+
+            case 6:
+                test_list.last();
+                break;
+
+            case 7:
+                test_list.print_list();
+                break;
+
+            case 8:
+                test_list.print_backwards();
+                break;            
+        }
+        test_list.print_list();
+        cout<<endl;
+    }
+    return 0;
 }
